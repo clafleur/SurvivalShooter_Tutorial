b4d2775b1bdee9a88fb0507dcab56cfdddc874bb
clafleur <clayton@texsstudio.com>
2016-12-02 14:46:04 -0800

    Initial Check In
    Setting up the environment and player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(cameRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0.0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        //check to see if player is walking
+        bool walking = h != 0.0f || v != 0.0f;
+    }
+}
